f53986211fd7a72cf0c1cd383c3869bb51225110
anaselmakyy <anasmikiel@gmail.com>
2024-01-16 00:47:51 +0100

    fifth

--- src/projects.jsx
+++ src/projects.jsx
@@ -28,7 +28,8 @@ export default function Projects() {
               streamlined styling. <br /> - ReactJS: Enables dynamic and interactive user
               interfaces.
             </p>
-            <a href=""
+            <a href="https://www.ayoubelouarzazi.com/"
+            target="_blank"
             className=" md:ml-[5%] text-primary bg-cyan p-[4%] rounded-3xl text-xl font-poppins font-bold hover:bg-emerald-600 hover:text-primary hover:cursor-pointer md:p-[2%] w-[25%] text-center">Visit It</a>
           </div>
         </div>
@@ -39,7 +40,8 @@ export default function Projects() {
             <p className="m-[10%] bg-slate-600 p-[4%] rounded-lg teck md:text-2xl  md:p-[4%] md:mr-[10%]">
               <span className="text-cyan">Description :</span> <br />  to-do list website, empowering users to effortlessly add, modify, and delete tasks. Demonstrated proficiency in frontend and backend technologies, ensuring a seamless and responsive user experience. Committed to creating functional and visually appealing solutions that enhance productivity and organization
             </p>
-            <a href=""
+            <a href="https://to-do-list-6u85.vercel.app/"
+            target="_blank"
             className=" md:ml-[5%] text-primary bg-cyan p-[4%] rounded-3xl text-xl font-poppins font-bold hover:bg-emerald-600 hover:text-primary hover:cursor-pointer md:p-[1%] w-[25%] md:w-[15%] text-center">Visit It</a>
           </div>
           <img src="./first.png" alt="" className="pictureprojects w-[80%] md:w-[30%] rounded-lg md:ml-[-17%]"/>
@@ -54,7 +56,8 @@ export default function Projects() {
             <p className="m-[10%] bg-slate-600 p-[4%]  rounded-lg teck md:text-2xl md:p-[8%] md:ml-[-5%]">
               <span className="text-cyan">Description:</span> <br /> Created a dynamic and engaging country-learning application featuring comprehensive information lookup, flag recognition games, and capital quizzes. Utilized a user-friendly interface to enhance the learning experience and foster geographical knowledge. Demonstrated skills in full-stack development, design, and educational content creation.
             </p>
-            <a href=""
+            <a href="https://flag-game-omega.vercel.app/"
+          target="_blank"
             className=" md:ml-[5%] text-primary bg-cyan p-[4%] rounded-3xl text-xl font-poppins font-bold hover:bg-emerald-600 hover:text-primary hover:cursor-pointer md:p-[1%] w-[25%] md:w-[15%] text-center">Visit It</a>
           </div>
         </div>
